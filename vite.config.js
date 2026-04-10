766afad3536e8fca3c3e51b5d1809ad0ce93f6ec
tajmhelly <oswaldmiller12@gmail.com>
2026-04-10 09:13:17 +0000

    Features

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  plugins: [tailwindcss()],
-
+  plugins: [react(), tailwindcss()],
 })
